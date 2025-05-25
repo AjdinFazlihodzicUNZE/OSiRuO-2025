427ba34674ec451c6446367e23c946788ff95b22
AjdinFazlihodzicUNZE <ajdin.fazlihodzic.24@size.ba>
2025-05-25 13:39:03 +0200

    Added dockerfile

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const roomsRoute = require('./routes/roomsRoute')
 const usersRoute = require('./routes/usersRoute')
 const bookingsRoute = require('./routes/bookingsRoute')
 app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Dobrodošli na projekat OSiRuO 2025!');
+});
 app.use('/api/rooms' , roomsRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/bookings', bookingsRoute)
